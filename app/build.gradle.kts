fb490bfd1c407c6ef68e56cdb9b1ff17adbe4ade
andersbor <anbo@zealand.dk>
2025-09-02 21:18:20 +0200

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.example.radiobuttonexample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.radiobuttonexample"
--- app/src/main/java/com/example/radiobuttonexample/MainActivity.kt
+++ app/src/main/java/com/example/radiobuttonexample/MainActivity.kt
@@ -86,7 +86,7 @@ fun RadioButtonGroup(
                     selected = (text == selectedOption),
                     onClick = null // null recommended for accessibility with screenreaders
                 )
-                Spacer(Modifier.width(16.dp))
+                Spacer(Modifier.width(8.dp))
                 Text(
                     text = text,
                     style = MaterialTheme.typography.bodyLarge,
